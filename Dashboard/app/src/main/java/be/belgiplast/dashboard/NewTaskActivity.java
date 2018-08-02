3247fd5133daa96d19e3376bf1f6068cbae347c9
Benoit Callebaut <benoit.callebaut@gmail.com>
2018-08-02 21:43:10 +0200

    work on new task activity

--- Dashboard/app/src/main/java/be/belgiplast/dashboard/NewTaskActivity.java
+++ Dashboard/app/src/main/java/be/belgiplast/dashboard/NewTaskActivity.java
@@ -2,6 +2,9 @@ package be.belgiplast.dashboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import be.belgiplast.library.tasks.AbstractTask;
 
@@ -13,8 +16,15 @@ public class NewTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_task);
 
         final AbstractTask task = new AbstractTask();
-        ((DashboardApplication)getApplicationContext()).getCoach().addTask(task);
+        ((EditTaskView)findViewById(R.id.task_editor)).setTask(task);
 
+        Button create = (Button)findViewById(R.id.button5);
+        create.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view) {
+                ((DashboardApplication)getApplicationContext()).getCoach().addTask(task);
+            }
+        });
     }
 }
